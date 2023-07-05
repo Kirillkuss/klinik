98432385d8c577aa2887c8791dd6c4cce77f1063
barysevich_k <99822514+Kirillkuss@users.noreply.github.com>
2023-07-05 16:22:06 +0300

    sdfsd

--- src/main/java/com/klinik/KlinikApplication.java
+++ src/main/java/com/klinik/KlinikApplication.java
@@ -3,16 +3,14 @@ package com.klinik;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication; 
 
+@Slf4j 
 @SpringBootApplication
 @OpenAPIDefinition( info = @Info( title = "КЛИНИК АПИ",
                                   version = "1.0.0",
                                   description = "КЛИНИК АПИ"))
-
-@Slf4j 
 public class KlinikApplication {
 
     public static void main(String[] args) {
