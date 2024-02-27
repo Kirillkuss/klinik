090fd2f806142a30b4419ff15c95e1bf30ae87bf
barysevich_k <99822514+Kirillkuss@users.noreply.github.com>
2024-02-27 11:07:48 +0300

    update

--- src/main/java/com/klinik/cassandra/config/CassandraConfig.java
+++ src/main/java/com/klinik/cassandra/config/CassandraConfig.java
@@ -1,13 +1,11 @@
 package com.klinik.cassandra.config;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 
 @Configuration
 @PropertySource(value = { "classpath:cassandra.properties" })
-@ComponentScan("com.klinik.cassandra")
 @EnableCassandraRepositories( basePackages = "com.klinik.cassandra.repository")
 public class CassandraConfig {
 
--- src/main/java/com/klinik/redis/repository/DoctorRepositoryRedis.java
+++ src/main/java/com/klinik/redis/repository/DoctorRepositoryRedis.java
@@ -1,9 +1,10 @@
 package com.klinik.redis.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import com.klinik.redis.model.Doctor;
 
-
+@Repository
 public interface DoctorRepositoryRedis extends CrudRepository<Doctor, String> {
     
 }
