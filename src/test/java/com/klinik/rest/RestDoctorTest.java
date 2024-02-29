a1ad2b8db6c298d59b085426f9a141c8a9a3833b
barysevich_k <99822514+Kirillkuss@users.noreply.github.com>
2024-02-29 10:25:46 +0300

    updatetest

--- src/test/java/com/klinik/rest/RestDoctorTest.java
+++ src/test/java/com/klinik/rest/RestDoctorTest.java
@@ -2,17 +2,17 @@ package com.klinik.rest;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
 import io.qameta.allure.Allure;
 import io.qameta.allure.Description;
-import io.qameta.allure.Features;
+import io.qameta.allure.Link;
 import io.qameta.allure.Step;
-import io.qameta.allure.Stories;
 import io.qameta.allure.TmsLink;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
@@ -22,45 +22,38 @@ import io.qameta.allure.Feature;
 @DisplayName("Тест АПИ для сущности Doctor")
 public class RestDoctorTest {
 
-   /**  @Test
-    public void testGetDoctorCounts() {
-        given().when().get("http://localhost:8082/web/doctors/counts").then().statusCode(200);
-    }*/
-
     @Test
-   // @Feature("Тест АПИ для сущности Doctor")
     @Description("Получение количества врачей 2")
     @DisplayName("Получение количества врачей 2")
     @TmsLink("TEST-1234")
+    @Link(name = "swagger", url = "http://localhost:8082/web/swagger-ui/index.html#/1.%20Doctors/getCountDoctors")
     public void testGetDoctorCountsTWo() {
         when().request("GET", "http://localhost:8082/web/doctors/counts").then().statusCode(200);
-       // Allure.addAttachment("Результат:", "text/plain", response.andReturn().asString() );
     } 
 
     @Test
-   // @Feature("Тест АПИ для сущности Doctor 1")
     @Description("Получение количества врачей 1")
     @DisplayName("Получение количества врачей 1")
+    @Link(name = "swagger", url = "http://localhost:8082/web/swagger-ui/index.html#/1.%20Doctors/getCountDoctors")
     @TmsLink("TEST-3545")
     public void testGetDoctorCounts() {
         RestAssured.baseURI = "http://localhost:8082";
         Response response = given().when().get("/web/doctors/counts");
         response.then().statusCode(200);
-        //System.out.println( "RESPONSE >>>" + response.andReturn().asString() );
         Allure.addAttachment("Результат:", "text/plain", response.andReturn().asString() );
     }
 
-   // @Feature("Тест АПИ для сущности Doctor ")
     @Description("Получение списка врачей")
     @DisplayName("Получение списка врачей")
     @ParameterizedTest
+    @Link(name = "swagger", url = "http://localhost:8082/web/swagger-ui/index.html#/1.%20Doctors/getAllDoc")
     @CsvSource({"1, 10", "5, 30", "40, 4"})
     @Step("\"Получение списка врачей страница -> {page} и размер -> {size}")
     public void testGetDocAll( int page, int size ){
         RestAssured.baseURI = "http://localhost:8082";
         Response response = given().when().get("/web/doctors?page=" + page + "&size=" + size );
         response.then().statusCode(200);
-        //Allure.addAttachment("Результат:", "text/plain", response.andReturn().asString() );
+        Allure.addAttachment("Результат:", "text/plain", response.andReturn().asString() );
     }
 
 }
