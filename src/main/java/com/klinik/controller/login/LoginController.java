207a643957d5bd85319656b3426275228a5277fb
barysevich_k <99822514+Kirillkuss@users.noreply.github.com>
2024-07-17 16:46:57 +0300

    refactor

--- src/main/java/com/klinik/controller/login/LoginController.java
+++ src/main/java/com/klinik/controller/login/LoginController.java
@@ -1,33 +1,31 @@
-package com.klinik.controller;
+package com.klinik.controller.login;
 
 import java.util.HashMap;
 import java.util.Map;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ResolvableType;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequiredArgsConstructor
 public class LoginController {
 
     private static final String authorizationRequestBaseUri = "oauth2/authorize-client";
-    Map<String, String> oauth2AuthenticationUrls = new HashMap<>();
+    private final ClientRegistrationRepository clientRegistrationRepository;
 
-    @Autowired
-    private ClientRegistrationRepository clientRegistrationRepository;
-
+    @SuppressWarnings("unchecked")
     @GetMapping("/login")
     public String getLoginPage(Model model) {
         Iterable<ClientRegistration> clientRegistrations = null;
-        ResolvableType type = ResolvableType.forInstance(clientRegistrationRepository)
-            .as(Iterable.class);
+        Map<String, String> oauth2AuthenticationUrls = new HashMap<>();
+        ResolvableType type = ResolvableType.forInstance(clientRegistrationRepository).as(Iterable.class);
         if (type != ResolvableType.NONE && ClientRegistration.class.isAssignableFrom(type.resolveGenerics()[0])) {
             clientRegistrations = (Iterable<ClientRegistration>) clientRegistrationRepository;
         }
-
         clientRegistrations.forEach(registration -> oauth2AuthenticationUrls.put(registration.getClientName(), authorizationRequestBaseUri + "/" + registration.getRegistrationId()));
         model.addAttribute("urls", oauth2AuthenticationUrls);
 
