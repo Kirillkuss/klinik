f9d7dc1d324cd963e94db7dedece6fcb96ce5513
barysevich_k <99822514+Kirillkuss@users.noreply.github.com>
2024-10-04 17:15:18 +0300

    add kafka

--- src/main/java/com/klinik/security/auth/SecurityConfiguration.java
+++ src/main/java/com/klinik/security/auth/SecurityConfiguration.java
@@ -2,7 +2,6 @@ package com.klinik.security.auth;
 
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
